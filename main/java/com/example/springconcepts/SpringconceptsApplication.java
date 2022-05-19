476053b7032d8597320edad8a73e05712466b56a
Karthik <jeefkarthik036@gmail.com>
2022-05-19 08:56:47 +0530

    Added logging framework

--- main/java/com/example/springconcepts/SpringconceptsApplication.java
+++ main/java/com/example/springconcepts/SpringconceptsApplication.java
@@ -2,18 +2,21 @@ package com.example.springconcepts;
 
 import com.example.springconcepts.component.DemoBean;
 import com.example.springconcepts.controller.HelloRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringconceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringconceptsApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to spring concepts");
+		logger.debug("Welcome to spring concepts");
 		ApplicationContext context = SpringApplication.run(SpringconceptsApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("Demo Bean = "+demoBean.toString());
+		logger.debug("Demo Bean = "+demoBean.toString());
 		System.out.println(context.getBean(HelloRestController.class));
 	}
 
